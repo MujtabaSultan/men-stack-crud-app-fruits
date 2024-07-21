3ace54e84bedd5da4bebb57f3780d74962c27b0f
Mujtaba_Sultan <b.mojtabaali@gmail.com>
2024-07-21 12:34:33 +0300

    added template

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ const morgan = require("morgan");
 
 const app = express();
 
+//middleware
 app.use(morgan("dev"));
+
+// server.js
+
+//routes
+
+// landing page
+app.get("/", async (req, res, next) => {
+  res.render("index.ejs");
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
